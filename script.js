b0730512633f26673dc080937d970bbfb669c3b2
Rasul <rasuldevv@gmail.com>
2023-07-12 10:06:01 +0500

    commit_2

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const nav_list = document.querySelector('.nav_list'),
-extra_btn = document.querySelector('.extra_btn')
+extra_btn = document.querySelector('.extra_btn'),
+nav_items = document.querySelectorAll('.nav_item');
 
 
 extra_btn.addEventListener('click', () => {
     nav_list.style.display = 'flex';
 })
 
-document.querySelector('.nav_item').addEventListener('click', () => {
-    nav_list.style.display = 'none';
-})
+// if(extra_btn.style.display === 'block'){
+//     nav_items.forEach(item => {
+//         item.addEventListener('click', () => {
+//             nav_list.style.display = 'none';
+//         })
+//     })
+// }
